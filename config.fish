ef40ba3e0db9bf51aede7cfe8f3aa2b617a3bfad
Humberto Ragazzini <79940052+humbertoragazzini@users.noreply.github.com>
2025-08-02 00:32:05 +0000

    Update config.fish
    small change in the config

--- config.fish
+++ config.fish
@@ -1,6 +1,10 @@
-#iDefine mybat alias
+# Define mybat alias
 alias mybat "upower -i (upower -e | grep BAT)"
 
+# Load nvm.fish
+nvm use node > /dev/null
+
+# Interactive session block
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
